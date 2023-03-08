a747cea780cffc9955e05f4d64d4c793382241ed
Naverolek <sebaskiba11@gmail.com>
2023-03-08 14:59:24 +0100

    Działający twig

--- pub/index.php
+++ pub/index.php
@@ -4,7 +4,17 @@ require_once('./../src/config.php');
 use Steampixel\Route;
 
 Route::add('/', function() {
-    echo "Strona główna";
+    //strona wyświetlająca obrazki
+    //echo "Strona główna";
+    global $twig;
+    $twig->display("index.html.twig");
+});
+
+Route::add('/upload', function() {
+    //wgrywanie obrazków    
+    global $twig;
+    $twig->display("upload.html.twig");
+
 });
 
 Route::run('/zadanieMemy/pub');
--- src/config.php
+++ src/config.php
@@ -2,5 +2,7 @@
 require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "cms_ss");
 require("Post.class.php");
+$loader = new Twig\Loader\FilesystemLoader("./../src/templates");
+$twig = new Twig\Environment($loader);
 
 ?>
