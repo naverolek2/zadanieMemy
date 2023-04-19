c2e96dbe826bf89c1bbefc6260621ace1cdd5892
Naverolek <sebaskiba11@gmail.com>
2023-04-19 14:53:37 +0200

    zaktualizowanie kopii zapasowej bazy

--- cms_ss.sql
+++ cms_ss.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 18 Kwi 2023, 15:52
--- Wersja serwera: 10.4.27-MariaDB
--- Wersja PHP: 8.2.0
+-- Czas generowania: 19 Kwi 2023, 14:53
+-- Wersja serwera: 10.4.24-MariaDB
+-- Wersja PHP: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,26 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `liked`
---
-
-CREATE TABLE `liked` (
-  `user_id` int(255) NOT NULL,
-  `post_id` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Zrzut danych tabeli `liked`
---
-
-INSERT INTO `liked` (`user_id`, `post_id`) VALUES
-(5, 4),
-(5, 3),
-(5, 2);
-
--- --------------------------------------------------------
-
---
 -- Struktura tabeli dla tabeli `post`
 --
 
@@ -52,20 +32,18 @@ CREATE TABLE `post` (
   `timestamp` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `filename` varchar(1024) NOT NULL,
   `title` text NOT NULL,
-  `likes` int(255) NOT NULL,
+  `likes` int(255) NOT NULL DEFAULT 0,
   `removed` tinyint(1) NOT NULL DEFAULT 0,
   `userID` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `post`
 --
 
 INSERT INTO `post` (`ID`, `timestamp`, `filename`, `title`, `likes`, `removed`, `userID`) VALUES
-(1, '2023-03-22 15:42:32', 'img/ea036693156016150666650c7e570c3bb1264ce9a1f8265c642692f87d4727f8.webp', 'geg', 0, 0, 3),
-(2, '2023-04-18 15:52:30', 'img/4458378570289dc7a210da4a1d6b78ff0dabea878f0a40acb41a8fae758668ac.webp', 'hhh', 1, 0, 3),
-(3, '2023-04-18 15:44:54', 'img/3fbce6c1a59c6af4515a631a04664a710bb88e9c2945209ba1b0158a09cb9fea.webp', 'hree', 1, 0, 3),
-(4, '2023-04-18 15:44:26', 'img/5fdbe1dfc38ad1672fe4b49352a200a18b0c7e5a2ac7baf2778ac61792b89ca5.webp', 'essa123123', 1, 0, 3);
+(4, '2023-04-19 14:46:28', 'img/5fdbe1dfc38ad1672fe4b49352a200a18b0c7e5a2ac7baf2778ac61792b89ca5.webp', 'essa123123', 1, 0, 3),
+(10, '2023-04-19 14:49:36', 'img/bc17617da5ed6c92e3a5c5586d3def8921fc68e799cd0cfa21f41f84f26c8b75.webp', 'g43j45j45j45j', 1, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -77,7 +55,7 @@ CREATE TABLE `user` (
   `ID` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `user`
@@ -88,7 +66,11 @@ INSERT INTO `user` (`ID`, `email`, `password`) VALUES
 (2, 'geg@geg.crf', '$argon2i$v=19$m=65536,t=4,p=1$TTlNa0JmY3ZYblRwTlNhdA$09rR0RIjHxEYW5ZHrmtQ1G0anfx+p6r8s01JJdQuDmg'),
 (3, 'a@a.a', '$argon2i$v=19$m=65536,t=4,p=1$eDhpRmowVTlMeHFxcy9MSg$9P8ok0tkSKqtnZTx6+w4uQvhUaaH8vaJ2mGsll0fZbs'),
 (4, 'q@q.q', '$argon2i$v=19$m=65536,t=4,p=1$LmRLVHdWTEJFMTFsTTV1RQ$DmbCzK+Eu26ydq6wDskyQgWX9p9GB3ooOt/pbQK22og'),
-(5, 'z@z.z', '$argon2i$v=19$m=65536,t=4,p=1$TXcvYUhOb2R3eUNSMFBsMA$yGB2s8oAz4vbevA0Bt4rj97t+ctVUltsuZruP5YqhVo');
+(5, 'z@z.z', '$argon2i$v=19$m=65536,t=4,p=1$TXcvYUhOb2R3eUNSMFBsMA$yGB2s8oAz4vbevA0Bt4rj97t+ctVUltsuZruP5YqhVo'),
+(6, 'm@m.m', '$argon2i$v=19$m=65536,t=4,p=1$b2tFRFlzSzZvZURmd3VNRA$HORhXjWuFZzynbPvg8FJQiG7WPDx5s7jdnVaZoWQSAY'),
+(7, '1@1.1', '$argon2i$v=19$m=65536,t=4,p=1$NDVhUjc3Mml2WWpzQ1dXZw$6E1xrkR4cr6w/LQq6ElHUrLzRmXIva5Y6AofLbwMJIU'),
+(8, '2@2.2', '$argon2i$v=19$m=65536,t=4,p=1$bzV1ZWw3Z25MYlJMenBVeQ$Uy8/0aYAKib3e4OIVV2Q94qm+78hCTrrkOP535n+uVk'),
+(9, '3@3.3', '$argon2i$v=19$m=65536,t=4,p=1$MFdxRi81U1ovS1IxaFBYdA$PUAWpAnlQC8mh99zp1c0LuLldaHxt6SsKgp1OvczWc8');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -115,13 +97,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT dla tabeli `post`
 --
 ALTER TABLE `post`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT dla tabeli `user`
 --
 ALTER TABLE `user`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
